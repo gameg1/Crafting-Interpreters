d51fd5d99886f1b12e11495113a647d265b971f5
Callum Staras <Callum.staras@googlemail.com>
2026-06-07 16:09:30 +0100

    Implemented runFile

--- Pylox/pylox.py
+++ Pylox/pylox.py
@@ -7,15 +7,20 @@ args = parser.parse_args()
 
 def main():
     arg_inputs = args.input
-    if len(arg_inputs < 1):
+    # Prints usage if no input
+    if len(arg_inputs) < 1:
         print("Usage: pylox [script]")
         exit(64)
+    # Runs the file if given a path
     elif (len(arg_inputs) == 1):
         runFile(arg_inputs[0])
+    # runs the prompt
     else:
         runPrompt()
 
-
+def runFile(path:str) -> None:
+    with open(path) as file:
+        run(file.read())
 
 if __name__ == '__main__':
     main()
